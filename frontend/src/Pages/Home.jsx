5429dea97c8855d9e3ed605eae3117b7ec4eeecc
rahulk67 <rahulkumawat6767@gmail.com>
2025-07-17 21:14:44 +0530

    add kyc and 1499

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -684,7 +684,7 @@ function Home() {
 
       <div
         ref={gameAreaRef}
-        className="game-area d-none"
+        className="game-area "
         style={
           {
             // transform: `translateX(-${currentStep * 85}px)`,
